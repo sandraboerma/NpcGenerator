5840f93f0e2015103dc43285d6b0a82ec09bfe39
sandraszu <sandra.szu@iths.se>
2025-01-03 01:55:24 +0100

    Initial commit

---
+++ src/test/java/com/boerma/npcgenerator/NpcGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boerma.npcgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
